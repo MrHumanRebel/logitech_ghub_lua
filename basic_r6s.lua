5f0d2f8a53a1f12eed10dde4ce1e0d7b264c31d0
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2023-07-15 11:05:14 +0200

    Update basic_r6s.lua

--- basic_r6s.lua
+++ basic_r6s.lua
@@ -1,17 +1,19 @@
 EnablePrimaryMouseButtonEvents(true);
-x = 0;
- 
+x = 1;
+vertical_control = -1; -- -1
+horizontal_control = 5; -- 5
+
 function OnEvent(event, arg)
 	if IsKeyLockOn("numlock" )then
 		if IsMouseButtonPressed(3)then
 			repeat	
 				if IsMouseButtonPressed(1) then
 					repeat
-						MoveMouseRelative(0,6)
+						MoveMouseRelative(0,horizontal_control)
 						x = x + 1
 						Sleep(1)
 						if (x % 2) == 0 then
-						  MoveMouseRelative(-1,0)
+						  MoveMouseRelative(vertical_control,0)
 						end
 					until not IsMouseButtonPressed(1)
 					x = 0
