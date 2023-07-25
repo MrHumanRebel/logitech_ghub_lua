9717023644e8891068856cb8d733d7d36d69cb4c
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2023-07-25 16:03:15 +0200

    Update complex_r6s.lua

--- complex_r6s.lua
+++ complex_r6s.lua
@@ -1,27 +1,51 @@
 EnablePrimaryMouseButtonEvents(true);
 r = {} -- vertical_control, horizontal_control
 --######################################
-
 attacker = "hibana"
-defender = "rook"
-
+defender = "fenrir"
 --######################################
-r["amaru"] = {-1,4}; r["ash"] = {-1,7}; r["blackbeard"] = {-1,4}; r["brava"] = {-1,4}; r["buck"] = {-1,4}; r["capitao"] = {0,5}; r["dokkaebi"] = {-1,4}
-r["finka"] = {-1,4}; r["flores"] = {-1,4}; r["fuze"] = {-1,4}; r["glaz"] = {-1,4}; r["gridlock"] = {-1,4}; r["grim"] = {-1,4}; r["hibana"] = {-1,7}
-r["iana"] = {-1,4}; r["iq"] = {-1,4}; r["jackal"] = {-1,4}; r["kali"] = {-1,4}; r["lion"] = {-1,4}; r["maverick"] = {-1,5}; r["nokk"] = {-1,4}
-r["nomad"] = {-1,4}; r["osa"] = {-1,4}; r["sens"] = {-1,4}; r["sledge"] = {-1,4}; r["thatcher"] = {-1,4}; r["thermite"] = {-1,4}; r["twitch"] = {-1,10}
-r["ying"] = {-1,4}; r["zero"] = {-1,4}; r["zofia"] = {-1,4}
-
-r["alibi"] = {-1,4}; r["aruni"] = {-1,4}; r["azami"] = {-1,4}; r["bandit"] = {0,6}; r["castle"] = {-1,4}; r["caveira"] = {-1,4}; r["clash"] = {-1,4}
-r["doc"] = {-1,4}; r["echo"] = {-1,4}; r["ela"] = {-1,4}; r["fenrir"] = {-1,4}; r["frost"] = {-1,4}; r["goyo"] = {-1,4}; r["jager"] = {-1,4}
-r["kaid"] = {-1,4}; r["kapkan"] = {-1,4}; r["lesion"] = {-1,4}; r["maestro"] = {-1,4}; r["melusi"] = {-1,4}; r["mira"] = {1,8}; r["mozzie"] = {-1,4}
-r["mute"] = {-1,4}; r["oryx"] = {-1,4}; r["pulse"] = {-1,4}; r["rook"] = {-1,6}; r["smoke"] = {-1,4}; r["solis"] = {-1,4}; r["tachanka"] = {-1,4}
-r["thorn"] = {-1,4}; r["thunderbird"] = {-1,4}; r["valkyrie"] = {-1,4}; r["vigil"] = {-1,4}; r["wamai"] = {-1,4}; r["warden"] = {-1,4}
+r["amaru"] = {-1,9}; r["ash"] = {-1,7}; r["ace"] = {-1,6};
+r["alibi"] = {0,6}; r["aruni"] = {0,5}; r["azami"] = {-1,5};
+r["blackbeard"] = {1,3}; r["brava"] = {0,4}; r["buck"] = {-1,6};
+r["bandit"] = {0,7};
+r["capitao"] = {0,5}; r["dokka"] = {0,3};
+r["castle"] = {0,3}; r["caveira"] = {0,3}; r["clash"] = {-1,4};
+r["doc"] = {-1,5};
+r["echo"] = {-1,5}; r["ela"] = {1,7};
+r["finka"] = {-1,8}; r["flores"] = {0,4}; r["fuze"] = {-1,9};
+r["fenrir"] = {0,6}; r["frost"] = {0,4}; 
+r["glaz"] = {0,0}; r["gridlock"] = {-1,8}; r["grim"] = {-1,4};
+r["goyo"] = {-1,8};
+r["hibana"] = {-1,7};
+r["iana"] = {-1,4}; r["iq"] = {-1,4};
+r["jackal"] = {-1,8};
+r["jager"] = {-1,6};
+r["kali"] = {-1,4};
+r["kaid"] = {0,6}; r["kapkan"] = {-1,6};
+r["lion"] = {0,6}; 
+r["lesion"] = {0,8};
+r["maverick"] = {-1,5};
+r["maestro"] = {0,7}; r["melusi"] = {-1,5}; r["mira"] = {-1,8}; r["mozzie"] = {0,5}; r["mute"] = {0,6};
+r["nokk"] = {0,7}; r["nomad"] = {-1,6}; 
+r["osa"] = {2,3};
+r["oryx"] = {0,7};
+r["pulse"] = {-1,3};
+r["rook"] = {-1,6};
+r["sens"] = {0,5}; r["sledge"] = {1,4};
+r["smoke"] = {1,7}; r["solis"] = {0,5};
+r["thatcher"] = {-1,7}; r["thermite"] = {1,6}; r["twitch"] = {-1,10};
+r["tachanka"] = {1,3}; r["thorn"] = {0,5}; r["thunder"] = {-1,7};
+r["valkyrie"] = {0,5}; r["vigil"] = {-1,6};
+r["ying"] = {-1,6};
+r["zero"] = {-1,5}; r["zofia"] = {0,6};  
+r["wamai"] = {0,6}; r["warden"] = {-1,5};
 --######################################
 
 mod = "alt"
 mod2 = "ctrl"
+mod3 = "scrolllock"
 
+--######################################
 function OperatorSelector(operator)
   vertical_control = r[operator][1]
   horizontal_control = r[operator][2]
@@ -39,7 +63,7 @@ function OnEvent(event, arg)
 	if IsModifierPressed(mod) and IsModifierPressed(mod2) and IsMouseButtonPressed(3)then
          vertical_control, horizontal_control = OperatorSelector(defender)
 	end
-	if IsKeyLockOn("numlock") then
+	if IsKeyLockOn(mod3) then
 		if IsMouseButtonPressed(3) then
 			repeat	
 				if IsMouseButtonPressed(1) then
